a839b950718b2e8840521d927aad4892be7e6e32
wank <kunta0514@163.com>
2016-01-22 18:48:28 +0800

    提取HTML内容验证

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,6 +16,7 @@ class Kernel extends ConsoleKernel
         \App\Console\Commands\Inspire::class,
         \App\Console\Commands\SycnWorkflowProject::class,
         \App\Console\Commands\SycnTask::class,
+        \App\Console\Commands\Spider::class,
     ];
 
     /**
