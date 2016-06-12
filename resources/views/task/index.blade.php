b38ab8c382a7baf52de9ed0c779e95152c7da40d
zhuangsd <hbsnjzsd@163.com>
2016-06-12 18:12:40 +0800

    任务列表手机版优化

--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -22,18 +22,28 @@
     <div class="col-xs-12" style="margin-top: 20px;">
         <ul class="list-group">
             @foreach($tasks as $k=>$task)
-            <li class="list-group-item">
-                <strong>{{$k+1}}</strong>.
-                @if(stristr($task->ekp_task_type, 'BUG'))
-                    <span class="label label-danger">B</span>
-                @elseif(stristr($task->ekp_task_type, '咨询'))
-                    <span class="label label-info">咨</span>
-                @elseif(stristr($task->ekp_task_type, '需求'))
-                    <span class="label label-success">需</span>
-                @else
-                    <span class="label label-primary">{{mb_substr($task->ekp_task_type,0,1)}}</span>
-                @endif
-                {{$task->task_title}}
+            <li class="list-group-item" style="min-height: 50px;">
+                <div style="display: table;width: 100%">
+                <div class="div_left" style="width: 85%;float: left;">
+                    <strong>{{$k+1}}</strong>.
+                    @if(stristr($task->ekp_task_type, 'BUG'))
+                        <span class="label label-danger" style="padding: .1em .6em 0em;">B</span>
+                    @elseif(stristr($task->ekp_task_type, '咨询'))
+                        <span class="label label-info" style="padding: .1em .6em 0em;">咨</span>
+                    @elseif(stristr($task->ekp_task_type, '需求'))
+                        <span class="label label-success" style="padding: .1em .6em 0em;">需</span>
+                    @else
+                        <span class="label label-primary" style="padding: .1em .6em 0em;">{{mb_substr($task->ekp_task_type,0,1)}}</span>
+                    @endif
+                    {{$task->task_title}}
+                    <div class="red" style="font-weight:bolder;background-color: #ff3c00;color: #fff;font-size: 12px;margin-top: 5px;width:92px;padding: 1px;border-radius: 4px;text-align: center;">
+                        {{$task->task_no}}
+                    </div>
+                </div>
+                <div class="div_right" style="vertical-align:top;line-height: 25px;width:25px;float: right;text-align: center;border-radius: 100px;-moz-transition-duration: 0.8s;background-color: orange;color: white;font-weight: bold;">
+                    @if($task->developer) {!!substr(AppHelper::user_name($task->developer),0,3)!!}@endif
+                </div>
+                </div>
             </li>
             @endforeach
         </ul>
