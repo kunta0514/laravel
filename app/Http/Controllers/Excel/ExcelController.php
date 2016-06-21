f07aee15dc9f471c71d1794fe8a458a3778934b3
kunta0514 <kunta0514@163.com>
2016-06-21 15:52:58 +0800

    fix-excel-export-BUG

--- app/Http/Controllers/Excel/ExcelController.php
+++ app/Http/Controllers/Excel/ExcelController.php
@@ -106,6 +106,8 @@ class ExcelController extends Controller
         $query = null;
         $title = ['状态	','完成时间','PRI','任务编号','任务标题','客户名称','PM','工作流版本','开发人员','工作量','测试人员','工作量','EKP任务','实际任务','备注'];
 
+        $file_name = '任务明细'.date("Ymd",strtotime("now"));
+
         switch($type)
         {
             case 'year':
@@ -113,6 +115,7 @@ class ExcelController extends Controller
                 $tasks = Task::select('status','actual_finish_date','PRI','task_no', 'task_title','customer_name','abu_pm','erp_version','developer','developer_workload','tester','tester_workload','ekp_task_type','task_type','comment')
                     ->where('task_no','>',$query_begin)
                     ->get();
+                $file_name = '本年'.$file_name;
                 break;
             case 'month':
                 $query_begin = date("Ymd",mktime(0,0,0,date("m")-1,1,date("Y")));
@@ -122,24 +125,27 @@ class ExcelController extends Controller
                     ->where('task_no','>',$query_begin)
                     ->where('task_no','<',$query_end)
                     ->get();
+                $file_name = '本月'.$file_name;
                 break;
             case 'week':
                 $query_begin = date("Ymd",strtotime("-1 week Monday"));
                 $query_end = date("Ymd",strtotime("+0 week Monday"));
                 $tasks = Task::select('status','actual_finish_date','PRI','task_no', 'task_title','customer_name','abu_pm','erp_version','developer','developer_workload','tester','tester_workload','ekp_task_type','task_type','comment')
 //            ->where('developer_workload',0)
-                    ->where('task_no','like',$query.'%')
+                    ->where('task_no','>',$query_begin)
+                    ->where('task_no','<',$query_end)
                     ->get();
+                $file_name = '本周'.$file_name;
                 break;
             case 'yd':
                 $tasks = Task::select('status','actual_finish_date','PRI','task_no', 'task_title','customer_name','abu_pm','erp_version','developer','developer_workload','tester','tester_workload','ekp_task_type','task_type','comment')
                     ->where('abu_pm','刘嵩')
                     ->orderBy('task_no','DESC')
                     ->get();
+                $file_name = '本周'.$file_name;
                 break;
 
         }
-
 
         $cellData = [];
         $cellData = $tasks->toArray();
@@ -161,10 +167,51 @@ class ExcelController extends Controller
 //        die;
 //        print_r($cellData);
 
-        Excel::create('本月任务明细'.date("Ymd",strtotime("now")),function($excel) use ($cellData){
+        Excel::create($file_name,function($excel) use ($cellData){
             $excel->sheet('score', function($sheet) use ($cellData){
                 $sheet->rows($cellData);
             });
         })->export('xls');
+    }
+
+    protected function my_time()
+    {
+        echo date("Ymd",strtotime("now")), "\n";
+        echo date("Ymd",strtotime("-1 week Monday")), "\n";
+        echo date("Ymd",strtotime("-1 week Sunday")), "\n";
+        echo date("Ymd",strtotime("+0 week Monday")), "\n";
+        echo date("Ymd",strtotime("+0 week Sunday")), "\n";
+
+        echo "*********第几个月:";
+        echo date('n');
+        echo "*********本周周几:";
+        echo date("w");
+        echo "*********本月天数:";
+        echo date("t");
+        echo "*********";
+
+        echo '<br>上周起始时间:<br>';
+        echo date("Y-m-d H:i:s",mktime(0, 0 , 0,date("m"),date("d")-date("w")+1-7,date("Y"))),"\n";
+        echo date("Y-m-d H:i:s",mktime(23,59,59,date("m"),date("d")-date("w")+7-7,date("Y"))),"\n";
+        echo '<br>本周起始时间:<br>';
+        echo date("Y-m-d H:i:s",mktime(0, 0 , 0,date("m"),date("d")-date("w")+1,date("Y"))),"\n";
+        echo date("Y-m-d H:i:s",mktime(23,59,59,date("m"),date("d")-date("w")+7,date("Y"))),"\n";
+
+        echo '<br>上月起始时间:<br>';
+        echo date("Y-m-d H:i:s",mktime(0, 0 , 0,date("m")-1,1,date("Y"))),"\n";
+        echo date("Y-m-d H:i:s",mktime(23,59,59,date("m") ,0,date("Y"))),"\n";
+        echo '<br>本月起始时间:<br>';
+        echo date("Y-m-d H:i:s",mktime(0, 0 , 0,date("m"),1,date("Y"))),"\n";
+        echo date("Y-m-d H:i:s",mktime(23,59,59,date("m"),date("t"),date("Y"))),"\n";
+
+        $season = ceil((date('n'))/3);//当月是第几季度
+        echo '<br>本季度起始时间:<br>';
+        echo date('Y-m-d H:i:s', mktime(0, 0, 0,$season*3-3+1,1,date('Y'))),"\n";
+        echo date('Y-m-d H:i:s', mktime(23,59,59,$season*3,date('t',mktime(0, 0 , 0,$season*3,1,date("Y"))),date('Y'))),"\n";
+
+        $season = ceil((date('n'))/3)-1;//上季度是第几季度
+        echo '<br>上季度起始时间:<br>';
+        echo date('Y-m-d H:i:s', mktime(0, 0, 0,$season*3-3+1,1,date('Y'))),"\n";
+        echo date('Y-m-d H:i:s', mktime(23,59,59,$season*3,date('t',mktime(0, 0 , 0,$season*3,1,date("Y"))),date('Y'))),"\n";
     }
 }
